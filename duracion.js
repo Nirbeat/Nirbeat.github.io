c679ba9df0999d3617c3bd8a0af7971ac3dce0c9
Nirbeat <103467703+Nirbeat@users.noreply.github.com>
2022-08-15 15:09:17 -0300

    Update duracion.js

--- duracion.js
+++ duracion.js
@@ -1,22 +1,27 @@
 let calculo,reset,tiempoSec=0;
 let app=document.getElementById('app');
-
+let repertorio={};
+let arrNom=[];
+let arrDur=[];
 
 (function agregarOpciones(){
+    
     fetch('https://github.com/Nirbeat/repertorios/blob/e8e55d833923e371913527b859a33dd44a367744/tnc.json')
         .then(res=>res.json())
         .then(res=>{
 
+            ordenarLista(res);
+            ordenarDuracion(arrNom,res);
             for(let i=0;i<Object.entries(res).length;i++){
 
-                app.appendChild(document.createElement('span')).className="opcion";
-                app.lastChild.appendChild(document.createElement('input')).id=i;
-                document.getElementById(i).type="checkbox";
-                app.lastChild.appendChild(document.createElement('label')).innerHTML=res[`id${i}`].nombre;
-                document.getElementById(i).setAttribute("duracion",res[`id${i}`].durationSec);
-                app.lastChild.appendChild(document.createElement('br'));
-            }
-        })
+            app.appendChild(document.createElement('span')).className="opcion";
+            app.lastChild.appendChild(document.createElement('input')).id=i;
+            document.getElementById(i).type="checkbox";
+            app.lastChild.appendChild(document.createElement('label')).innerHTML=arrNom[i];
+            document.getElementById(i).setAttribute("duracion",arrDur[i]);
+            app.lastChild.appendChild(document.createElement('br'));
+            }   
+        });
 
         crearBoton('calcular');
 })();
@@ -34,33 +39,32 @@ function calcularTiempo(){
         }        
     }
 
-    convertirTiempo()
+    mostrarTiempo()
     document.getElementById('calculo').remove();
     crearBoton('reset');
 }
 
+
 function reinicio(){
     tiempoSec=0;
     app.lastChild.remove();
-
-    for(let i=0;i<app.childElementCount;i++){
-
-        let check=document.getElementById(i);
-        if(check.checked){
-            check.checked=false;
-        }
-    }
     document.getElementById('reset').remove();
     crearBoton('calcular');
 }
 
-function convertirTiempo(){
+
+function mostrarTiempo(){
     
     let min=Math.floor(tiempoSec/60);
     let sec=parseInt(tiempoSec%60);
     tiempoSec=min;
-    app.appendChild(document.createElement('p')).innerHTML=`EL show dura ${min}:${sec} minutos`
+
+    if(sec<10){
+        app.appendChild(document.createElement('p')).innerHTML=`EL show dura ${min}:0${sec} minutos`
+    }else (app.appendChild(document.createElement('p')).innerHTML=`EL show dura ${min}:${sec} minutos`)
+    
 }
+
 
 function crearBoton(arg){
 
@@ -78,6 +82,32 @@ function crearBoton(arg){
         document.body.lastChild.id='reset';
         document.body.lastChild.value='Reset';
         document.body.lastChild.addEventListener('click',reinicio);
+    } 
+}
+
+function ordenarLista(data){
+
+    for(let i=0;i<Object.entries(data).length;i++){
+
+        arrNom.push(data[`id${i}`].nombre);
     }
-    
+    arrNom.sort();
+}
+
+function ordenarDuracion(arr,data){
+
+    for(element of arr){
+
+        for(let i=0;i<arr.length;i++){
+
+            if(data[`id${i}`]['nombre']==element){
+                arrDur.push(data[`id${i}`].durationSec);
+            }
+        }
+    }
+}
+
+function elegirBanda(){
+
+   
 }
