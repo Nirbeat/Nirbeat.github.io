d2d77634305f39a26102a4ac0ba84e146fc9903a
Nirbeat <103467703+Nirbeat@users.noreply.github.com>
2022-08-31 16:46:58 -0300

    Update examen.js
    se ordenaron las funciones para que la lectura de la logica del codigo sea mas sencila
    
    se agregó la funcion de evaluacion y un alert informa del puntaje (de 0 a 10)
    
    en posteriores updates se va a limpiar el DOM y aparecerá un mensaje de felicitación con una devolucion más precisa para que el alumno estudie en particular sobre los puntos fallados

--- examen/examen.js
+++ examen/examen.js
@@ -1,19 +1,4 @@
-//LISTENERS
-function evento(tag,evento,funcion){
-    document.addEventListener('DOMContentLoaded',(e)=>{
-        if(!document.getElementById(tag)){
-            e.preventDefault
-        }else{
-            document.getElementById(tag).addEventListener(evento,funcion);
-        }
-    }); 
-} 
-
-evento('examen','click',crearConsignas);
-evento('evaluar','click',evaluar);
-
-
-//OBJETOS DEL EXAMEN
+//CLASE DE OBJETOS DEL EXAMEN
 class ObjetosExamen{
     constructor(ej1,ej2,ej3,ej4){
         this.ej1=ej1;
@@ -26,19 +11,10 @@ class ObjetosExamen{
 let objetoRespuesta=new ObjetosExamen();
 let objetoSolucion=new ObjetosExamen();
 
+//CREA PROPIEDADES DE OBJETOS DE EXAMEN
+function CrearProps(obj){
 
-
-//FUNCION PARA PONER LAS IMAGENES EN EL DOM
-function imagenesExamen(ejercicio,nroImagen){
-    let respuestas;
-    respuestas=JSON.parse(localStorage.getItem(`ej${ejercicio}`));
-    ruta=`imagen/bajo1/ej${ejercicio}/${respuestas[nroImagen]}.png`;
-    return ruta;
-};
-
-
-//CREA PROPIEDADES DE OBJETOS DE EXAMEN Y GUARDA EL LOCALSTORAGE LAS RESPUESTAS PARA ASIGNAR LAS IMAGENES
-function crearProps(obj){
+    //GUARDA EN LOCALSTORAGE EL INDICE DE LAS RESPUESTAS
     if(obj==objetoSolucion){
         let opciones;
         fetch('resp.json')
@@ -54,47 +30,31 @@ function crearProps(obj){
                 obj[`ej${i}`]=respuestasObjeto;
             }
         });
+
+    //TOMA LAS RESPUESTAS DEL DOM Y LAS GUARDA EN EL OBJETO
     }if(obj==objetoRespuesta){
-        tomarRespuestas();
-    }
-    return obj;
-}
-
-//CREA EL EXAMEN EN EL DOM
-
-//hay que revisar todo, sacar los br y dar un estilo mejor en lo posible con CSS
-function crearConsignas(){
-    let examen=document.getElementById('consigna');
-    let consignas=fetch('consignas.json');
-    consignas
-    .then(res=>res.json())
-    .then((res)=>{
+        
         for(let i=1;i<=4;i++){
-            elegirRespuestas(i);
-            let pregunta=document.createElement('p');
-            examen.appendChild(document.createElement('div'))
-                .appendChild(pregunta);
-            pregunta.innerHTML=res[i];
-            pregunta.appendChild(document.createElement('br'));
+            let arr=[];
             for(let j=1;j<=5;j++){
-                let span=document.createElement('span');
-                pregunta.appendChild(span);
-                pregunta.appendChild(document.createElement('br'));
-                span.appendChild(document.createElement('img'))
-                    .src=imagenesExamen(i,j-1)
-                span.appendChild(document.createElement('br'));
-                span.appendChild(document.createElement('input'))
-                    .id=`ej${i}${j}`;           
-            } 
-        }        
-    });examen.appendChild(document.createElement('button'))
-            .innerHTML='Corregir'
-            //.id='evaluar'
+                let respuesta=document.getElementById(`ej${i}${j}`).value;
+                arr.push(respuesta.toLowerCase());
+            }
+            objetoRespuesta[`ej${i}`]=arr;
+        }
+    }
 }
 
+
+//BOTON CREADOR DEL EXAMEN
+const examen=document.getElementById('examen');
+examen.addEventListener('click',()=>{
+    CrearConsignas();
+    examen.remove()
+});
 
 //ELEGIR IMAGENES POR NUMERO Y CREAR LOCALSTORAGE
-function elegirRespuestas(n){
+function ElegirRespuestas(n){
     let num=0;
     let solucion=[];
     for(let i=0;i<=4;i++){
@@ -112,15 +72,69 @@ function elegirRespuestas(n){
     }
 }
 
-//FUNCION PARA TOMAR RESPUESTAS
-function tomarRespuestas(){
+
+//FUNCION PARA PONER LA RUTA DE IMAGENES EN EL DOM
+function ImagenesExamen(ejercicio,nroImagen){
+    let respuestas;
+    respuestas=JSON.parse(localStorage.getItem(`ej${ejercicio}`));
+    ruta=`imagen/bajo1/ej${ejercicio}/${respuestas[nroImagen]}.png`;
+    return ruta;
+};
+
+//CREA EL EXAMEN EN EL DOM
+
+//hay que revisar todo, sacar los br y dar un estilo mejor en lo posible con CSS
+function CrearConsignas(){
+    let examen=document.getElementById('consigna');
+    let consignas=fetch('consignas.json');
+    consignas
+    .then(res=>res.json())
+    .then((res)=>{
+        for(let i=1;i<=4;i++){
+            ElegirRespuestas(i);
+            let pregunta=document.createElement('p');
+            examen.appendChild(document.createElement('div'))
+                .appendChild(pregunta);
+            pregunta.innerHTML=res[i];
+            pregunta.appendChild(document.createElement('br'));
+            for(let j=1;j<=5;j++){
+                let span=document.createElement('span');
+                pregunta.appendChild(span);
+                pregunta.appendChild(document.createElement('br'));
+                span.appendChild(document.createElement('img'))
+                    .src=ImagenesExamen(i,j-1)
+                span.appendChild(document.createElement('br'));
+                span.appendChild(document.createElement('input'))
+                    .id=`ej${i}${j}`;           
+            } 
+        }        
+    });
+    document.getElementById('buttons').appendChild(document.createElement('button')).id='corregir';
+    document.getElementById('corregir').innerHTML='Evaluarme!';
+
+    const evaluar=document.getElementById('corregir');
+    evaluar.addEventListener('click',Evaluar);
 
 }
 
-//CREACION DE OBJETOS A EVALUAR
-function evaluar(){
-    crearProps(objetoSolucion);
-    crearProps(objetoRespuesta);
+//COMPARA LAS RESPUESTAS Y EL ARRAY DE RESPUESTAS CORRECTAS
+function Evaluar() {
+    let puntaje=0;
+
+    CrearProps(objetoSolucion);
+    CrearProps(objetoRespuesta);
+
+    for(ej in objetoSolucion){
+
+        for(let i=0;i<=4;i++){
+            try {
+                if(objetoSolucion[ej][i]==objetoRespuesta[ej][i]){
+                puntaje++;
+                }
+            } catch (error) {
+                console.log('No se que le pasa, pero solo si pongo el try/catch se ejecuta como corresponde')
+            }  
+        }
     }
-
-
+    alert(`Tu puntaje es de ${puntaje/2}`);
+}
